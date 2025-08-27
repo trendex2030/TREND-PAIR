53e0f9231b896deb869d46e4ca811c3261aa9c23
trendex2030 <bravinnyakundi674@gmail.com>
2025-08-28 01:11:39 +0300

    Update pair.js

--- pair.js
+++ pair.js
@@ -65,42 +65,6 @@ let md = "TREND-XMD~" + base64Session;
 
 // Send session ID to your WhatsApp
 let code = await sock.sendMessage(sock.user.id, { text: md });
-                        let desc = `*Hey there, TREND-X User!* 👋🏻
-
-Thanks for using *TREND-X* — your session has been successfully created!
-
-🔐 *Session ID:* Sent above  
-⚠️ *Keep it safe!* Do NOT share this ID with anyone.
-
-——————
-
-*✅ Stay Updated:*  
-Join our official WhatsApp Channel:  
-https://whatsapp.com/channel/0029Vb6b7ZdF6sn4Vmjf2X1O
-
-*💻 Source Code:*  
-Fork & explore the project on GitHub:  
-git clone https://github.com/trendex2030/TREND-X
-
-——————
-
-> *© Powered by trend-x King*
-Stay cool and hack smart. ✌🏻`; 
-                        await sock.sendMessage(sock.user.id, {
-text: desc,
-contextInfo: {
-externalAdReply: {
-title: "TREND-X",
-thumbnailUrl: "https://files.catbox.moe/adymbp.jpg",
-sourceUrl: "https://whatsapp.com/channel/0029Vb6b7ZdF6sn4Vmjf2X1O",
-mediaType: 1,
-renderLargerThumbnail: true
-}  
-}
-},
-{quoted:code })
-                    } catch (e) {
-                            let ddd = sock.sendMessage(sock.user.id, { text: e });
                             let desc = `Hey there, TREND-X User!* 👋🏻
 
 Thanks for using *TREND-X* — your session has been successfully created!
