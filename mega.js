9223fe5a92ed3469cc4326b2efa1179f1a3b8df1
trendex2030 <bravinnyakundi674@gmail.com>
2026-02-21 11:10:06 +0300

    Change Mega account email and password
    Updated Mega account credentials for authentication.

--- mega.js
+++ mega.js
@@ -1,8 +1,8 @@
 const mega = require("megajs");
 
 const auth = {
-    email: 'popkidtelegram@gmail.com',   //use your real vaild mega account email
-    password: 'popkidtelegram',  ////use your real vaild mega account password
+    email: 'trendexunstoppable@gmail.com',   //use your real vaild mega account email
+    password: 'trendex@001',  ////use your real vaild mega account password
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
 };
 
